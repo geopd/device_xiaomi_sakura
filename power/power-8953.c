f4ecb7d43fe3635472df8579e99295c47c93a96a
Michael Bestas <mkbestas@lineageos.org>
2018-05-25 22:57:04 +0300

    sakura: power: msm8953: Support power profiles
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Fri May 25 22:57:04 2018 +0300
    
        power: msm8953: Add support for power profiles
    
        * Based on legacy HAL implementation
        * We no longer handle POWER_HINT_INTERACTION and POWER_HINT_LAUNCH,
          they're now supported directly by Qualcomm mp-ctl.
    
        Change-Id: If5b239a6fa1880e2e650f0e5c50c1357e8e3ac1a
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- power/power-8953.c
+++ power/power-8953.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016, 2018, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018-2021 The LineageOS Project
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -50,6 +51,91 @@
 #include "utils.h"
 
 static int video_encode_hint_sent;
+
+static int current_power_profile = PROFILE_BALANCED;
+
+// clang-format off
+static int profile_high_performance[] = {
+    SCHED_BOOST_ON_V3, 0x1,
+    ALL_CPUS_PWR_CLPS_DIS_V3, 0x1,
+    CPUS_ONLINE_MIN_BIG, 0x4,
+    MIN_FREQ_BIG_CORE_0, 0xFFF,
+    MIN_FREQ_LITTLE_CORE_0, 0xFFF,
+    GPU_MIN_POWER_LEVEL, 0x1,
+    SCHED_PREFER_IDLE_DIS_V3, 0x1,
+    SCHED_SMALL_TASK, 0x1,
+    SCHED_MOSTLY_IDLE_NR_RUN, 0x1,
+    SCHED_MOSTLY_IDLE_LOAD, 0x1,
+};
+
+static int profile_power_save[] = {
+    CPUS_ONLINE_MAX_BIG, 0x1,
+    MAX_FREQ_BIG_CORE_0, 0x3bf,
+    MAX_FREQ_LITTLE_CORE_0, 0x300,
+};
+
+static int profile_bias_power[] = {
+    MAX_FREQ_BIG_CORE_0, 0x4B0,
+    MAX_FREQ_LITTLE_CORE_0, 0x300,
+};
+
+static int profile_bias_performance[] = {
+    CPUS_ONLINE_MAX_BIG, 0x4,
+    MIN_FREQ_BIG_CORE_0, 0x540,
+};
+// clang-format on
+
+#ifdef INTERACTION_BOOST
+int get_number_of_profiles() {
+    return 5;
+}
+#endif
+
+void set_power_profile(int profile) {
+    int ret = -EINVAL;
+    const char* profile_name = NULL;
+
+    if (profile == current_power_profile) return;
+
+    ALOGV("%s: Profile=%d", __func__, profile);
+
+    if (current_power_profile != PROFILE_BALANCED) {
+        undo_hint_action(DEFAULT_PROFILE_HINT_ID);
+        ALOGV("%s: Hint undone", __func__);
+        current_power_profile = PROFILE_BALANCED;
+    }
+
+    if (profile == PROFILE_POWER_SAVE) {
+        ret = perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_power_save,
+                                  ARRAY_SIZE(profile_power_save));
+        profile_name = "powersave";
+
+    } else if (profile == PROFILE_HIGH_PERFORMANCE) {
+        ret = perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_high_performance,
+                                  ARRAY_SIZE(profile_high_performance));
+        profile_name = "performance";
+
+    } else if (profile == PROFILE_BIAS_POWER) {
+        ret = perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_bias_power,
+                                  ARRAY_SIZE(profile_bias_power));
+        profile_name = "bias power";
+
+    } else if (profile == PROFILE_BIAS_PERFORMANCE) {
+        ret = perform_hint_action(DEFAULT_PROFILE_HINT_ID, profile_bias_performance,
+                                  ARRAY_SIZE(profile_bias_performance));
+        profile_name = "bias perf";
+    } else if (profile == PROFILE_BALANCED) {
+        ret = 0;
+        profile_name = "balanced";
+    }
+
+    if (ret == 0) {
+        current_power_profile = profile;
+        ALOGD("%s: Set %s mode", __func__, profile_name);
+    } else {
+        ALOGE("Setting power profile failed. perf HAL not started?");
+    }
+}
 
 /**
  * Returns true if the target is SDM625.
@@ -159,6 +245,12 @@ static int process_activity_launch_hint(__attribute__((unused)) void* data) {
 
 int power_hint_override(power_hint_t hint, void* data) {
     int ret_val = HINT_NONE;
+
+    // Skip other hints in high/low power modes
+    if (current_power_profile == PROFILE_POWER_SAVE ||
+        current_power_profile == PROFILE_HIGH_PERFORMANCE) {
+        return HINT_HANDLED;
+    }
 
     switch (hint) {
         case POWER_HINT_VIDEO_ENCODE:
