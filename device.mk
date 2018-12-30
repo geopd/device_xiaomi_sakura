085d2920cab7c4acd0b5917899b1b8a00a6bf0b2
Bruno Martins <bgcngm@gmail.com>
2018-12-30 19:58:56 +0000

    sakura: Convert XiaomiDoze into XiaomiParts
     * This is the preparation for turning the package into
       an unified device-settings app.
    
    Change-Id: I425ad525edcc63c2dcccd1e2fb8459f442744970
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -92,6 +92,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# Device-specific Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
@@ -193,10 +197,6 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libnl \
     libtinyxml
-
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
 
 # DRM
 PRODUCT_PACKAGES += \
