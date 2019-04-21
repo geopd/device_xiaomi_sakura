46a7c8cd6c92b2b69524e423d75d6e858782814c
Nich <nctrenco@gmail.com>
2019-04-21 08:40:41 +0000

    sakura: rootdir: Use default allocstall threshold on k4.9
    Memory management has significantly improved on k4.9, and we've seen
    much lesser thrashing as compared to k3.18. There is now a need to be
    more aggressive under high memory pressure on k4.9 to reduce negative
    performance impact.
    
    Use the default threshold (70), but don't set it to 0 to avoid overly
    aggressive killings under low memory pressure.
    
    Change-Id: Ie989a9f71738cd764c07c7c723b31a808b6ca390
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -422,8 +422,17 @@ else
         esac
     fi
 
-    # Set allocstall_threshold to 0 for all targets.
-    echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
+    KernelVersionStr=`cat /proc/sys/kernel/osrelease`
+    KernelVersionS=${KernelVersionStr:2:2}
+    KernelVersionA=${KernelVersionStr:0:1}
+    KernelVersionB=${KernelVersionS%.*}
+
+    # Don't account allocstalls for <= 2GB RAM targets on kernel versions < 4.9
+    if [ $KernelVersionA -lt 4 ] || [ $KernelVersionB -lt 9 ]; then
+        if [ $MemTotal -le 2097152 ]; then
+            echo 100 > /sys/module/vmpressure/parameters/allocstall_threshold
+        fi
+    fi
 
     configure_read_ahead_kb_values
 
