eea729374029705d221446ec771f28848538a1d7
Dhina17 <dhinalogu@gmail.com>
2020-01-29 17:38:52 +0100

    sakura: Move ConfigStore Flags To System Properties
    * In Android 10, ConfigStore HAL is replaced by System properties
      But Still ConfigStore is alive to support legacy vendor partitions
    
    Change-Id: I05971c7c20e881222deafe6fead035c5950f8fef
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -475,7 +475,9 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 	
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays = true \
+    ro.surface_flinger.max_virtual_display_dimension = 4096
 
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
