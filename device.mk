1a750de52a43481eb60eaa9e9b8863f2e7b843be
Nicholas Lim <nicknitewolf@lineageos.org>
2020-05-18 17:06:12 +0800

    sakura: Drop FDE support entirely
    Change-Id: Ifecd15ee6c86a4571e5a7cc961372348455c4d5c
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -221,10 +221,6 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
