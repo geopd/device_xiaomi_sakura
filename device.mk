d198707a66c4b2994066cba72cf23eb66a5963de
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2020-06-01 13:13:08 +0530

    sakura: Remove deprecated android.hardware.thermal@1.0 HAL
    Remove deprecated android.hardware.thermal@1.0 HAL from
    device manifest.
    
    Bug: 154012150
    Change-Id: I884e6b96cac4ebc3abcfaf309388d526a30dc5bd
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -425,13 +425,6 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # Thermal
-PRODUCT_PACKAGES += \
-    thermal.msm8953
-
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
