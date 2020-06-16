44ad893bca5992767a3bcf98a9c6b4a9aa0163d1
ZeeLog <Danjel.Zammit@gmail.com>
2020-06-16 08:18:36 +0200

    sakura: gps: Update GPS from LA.UM.8.6.r1-04400-89xx.0
    * Move gps configs to vendor
    * config.fs: Restore slim_daemon access
    * Set GPS parameters using gps_debug.conf
    * lower debug levels
    * build additional product packages
    
    Change-Id: I70368abcdf9795530201b8e8a544d287ecefb37a
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -221,15 +221,12 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    libwifi-hal-ctrl
+    libwifi-hal-ctrl \
+    libcurl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(LOCAL_PATH)/configs/gps/gps_debug.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps_debug.conf
 
 # HALS
 SRC_AUDIO_HAL_DIR := hardware/qcom-caf/msm8996/audio
