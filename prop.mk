28accdcdc03d2a89045f68a3762603aaeee3d607
tangrobin <tangrobin@google.com>
2020-08-25 09:49:58 +0530

    sakura: prop: import surface flinger offsets from Sunfish
    GPU takes long time(>8ms) when app was launching and closing.
    This may because GPU is handling some heavy task like color conversion.
    By setting NegativeOffset as 5ms to allow SF have more time to handle
    when doing animation transaction and shift back 5ms with
    early_app_phase_offset_ns such that app still has the same amount of
    time to handle each frame.
    
    Test: 1. Open Chrome from homescreen
          2. Swipe vertically to go back to home screen
          3. Measure the gpu completion time and "FrameMissed" count from systrace result
    Bug: 147339695
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ib90aa74e95fe1f4eea97466ccd7fd84c26e84af5
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -213,6 +213,13 @@ ro.surface_flinger.set_idle_timer_ms=9000 \
 ro.surface_flinger.set_touch_timer_ms=5000 \
 ro.surface_flinger.use_smart_90_for_video=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=11600000 \
+debug.sf.early_app_phase_offset_ns=11600000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true \
