a29dcf29658e12b9e8b846f2376d9cde76f63028
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-11 00:04:16 +0530

    sakura: Set ro.build.fingerprint as well
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- lineage_sakura.mk
+++ lineage_sakura.mk
@@ -43,3 +43,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
