deb77528afc726f9109ba5f63d9ff6825f7a8f4a
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    sakura: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -218,7 +218,11 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    libhidltransport \
+    libhwbinder \
+    libhidltransport.vendor \
+    libhwbinder.vendor
 
 # HW crypto
 PRODUCT_PACKAGES += \
