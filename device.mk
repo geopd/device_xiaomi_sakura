68b4e263ab19864a3f094a9b18d69ea1c3ef9493
Jiyong Park <jiyong@google.com>
2020-09-13 10:43:39 +0600

    sakura: Move VNDK APEX to vendor partition
    ... for aosp_* targets so that we don't need to update vendor.img everytime
    there is a change in the VNDK libraries
    
    Bug: 140136207
    Test: m
    Change-Id: Ic7ffaa9503902884f219400cf93516a6fbf8b210
    (cherry picked from commit 1d97f7055550d375aa7f371e148c3ec9aff0bd29)
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -430,7 +430,8 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package \
-    libstdc++.vendor
+    libstdc++.vendor \
+    com.android.vndk.current.on_vendor
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so \
