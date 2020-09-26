0e2242aa25df0d20f6237a46af919e99da57532c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-09-26 04:15:15 +0000

    sakura: Disable Blurs by default
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -228,6 +228,7 @@ debug.sf.earlyGl.app.duration=21000000
 
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.supports_background_blur=1 \
+persist.sys.sf.disable_blurs=1 \
 ro.sf.blurs_are_expensive=1
 
 # Time Services
