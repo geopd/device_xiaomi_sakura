65bf4913c0eb03bc7278ffb5cb8bbec098ddce25
geopdgitlab <geoemmanuelpd2001@gmail.com>
2020-09-28 14:24:33 +0000

    sakura: add prebuilts libprotobuf from VNDK 29
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -407,6 +407,9 @@ PRODUCT_PACKAGES += \
     vndk_package \
     libstdc++.vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
 
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
