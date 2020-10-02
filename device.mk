e851e6b473726e002deb041cd82f71af45ae2cd0
geopdgitlab <geoemmanuelpd2001@gmail.com>
2020-10-02 18:34:27 +0000

    sakura: Import basic USB HAL 1.0
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -415,7 +415,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.sakura
 
 # VNDK
 PRODUCT_PACKAGES += \
