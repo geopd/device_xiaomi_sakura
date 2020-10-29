435bc3e4b26750ee7c5564b468769a88df2cfccd
Baldev Sahu <bsahu@codeaurora.org>
2020-10-29 14:26:09 +0100

    sakura: Disable ahardware buffer
    - Disable ahardware buffer usage in gralloc
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -109,6 +109,7 @@ ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=0 \
+vendor.gralloc.disable_ahardware_buffer=1 \
 vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
