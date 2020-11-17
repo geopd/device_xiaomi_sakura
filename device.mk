876a342a542282b95b99bc2fe491d383db0c93ec
GeoPD <geoemmanuelpd2001@gmail.com>
2020-11-17 10:55:16 +0530

    sakura: Build some OMX packages
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -324,10 +324,12 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxCore \
     libOmxEvrcEnc \
+    libOmxG711Enc \
     libOmxQcelp13Enc \
     libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
+    libstagefright_softomx \
     libstagefrighthw \
     libstagefright_omx.vendor
 
