e1a15f53da034bd9ee97598b21db74865758e28f
SakilMondal <mondalsakil@gmail.com>
2020-11-21 22:51:50 +0100

    sakura: Build QTI thermal 2.0 hal
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -417,6 +417,9 @@ PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/sakura
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
