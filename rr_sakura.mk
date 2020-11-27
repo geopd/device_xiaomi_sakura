2b3500fd27d77c58cce778acf6b5448426132d85
GeoPD <geoemmanuelpd2001@gmail.com>
2020-11-27 11:14:31 +0530

    sakura: BringUp RR
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- rr_sakura.mk
+++ rr_sakura.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from MiuiCamera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
-# Inherit from mido device
+# Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResurrectionRemix OS stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
-PRODUCT_NAME := lineage_sakura
+PRODUCT_NAME := rr_sakura
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 6 Pro
