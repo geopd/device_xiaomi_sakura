45b7fd11ad7f2ce5fab8682febdd3adee5c98027
Stallix <joey@evolution-x.org>
2020-12-26 13:03:41 +0530

    sakura: init: Fix init_msm8953 formatting
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -1,31 +1,17 @@
 /*
-   Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2019, The LineageOS Project
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <cstdlib>
@@ -42,65 +28,57 @@
 #include "property_service.h"
 #include "vendor_init.h"
 
-void property_override(char const prop[], char const value[])
+void property_override(char const prop[], char const value[], bool add = true)
 {
-	prop_info *pi;
-	pi = (prop_info *)__system_property_find(prop);
-	if (pi)
-		__system_property_update(pi, value, strlen(value));
-	else
-		__system_property_add(prop, strlen(prop), value, strlen(value));
+    auto pi = (prop_info *) __system_property_find(prop);
+
+    if (pi != nullptr) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
 }
 
 void property_override_multifp(char const buildfp[], char const systemfp[],
-	char const bootimagefp[], char const vendorfp[], char const value[])
-{
+        char const bootimagefp[], char const vendorfp[], char const value[]) {
     property_override(buildfp, value);
     property_override(systemfp, value);
-    property_override(vendorfp, value);
     property_override(bootimagefp, value);
+    property_override(vendorfp, value);
 }
 
-void load_dalvik_properties()
-{
-	struct sysinfo sys;
+void load_dalvik_properties() {
+    struct sysinfo sys;
 
-	sysinfo(&sys);
-
-	if (sys.totalram > 3072ull * 1024 * 1024)
-	{
-		// from - phone-xxhdpi-4096-dalvik-heap.mk
-		property_override("dalvik.vm.heapstartsize", "8m");
-		property_override("dalvik.vm.heaptargetutilization", "0.6");
-		property_override("dalvik.vm.heapgrowthlimit", "192m");
-		property_override("dalvik.vm.heapsize", "512m");
-		property_override("dalvik.vm.heapmaxfree", "16m");
-		property_override("dalvik.vm.heapminfree", "4m");
-	}
-	else
-	{
-                // from - phone-xhdpi-2048-dalvik-heap.mk
-		property_override("dalvik.vm.heapstartsize", "8m");
-		property_override("dalvik.vm.heaptargetutilization", "0.7");
-		property_override("dalvik.vm.heapgrowthlimit", "192m");
-		property_override("dalvik.vm.heapsize", "512m");
-		property_override("dalvik.vm.heapmaxfree", "8m");
-		property_override("dalvik.vm.heapminfree", "2m");
-	}
+    sysinfo(&sys);
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xxhdpi-4096-dalvik-heap.mk
+	property_override("dalvik.vm.heapstartsize", "8m");
+	property_override("dalvik.vm.heaptargetutilization", "0.6");
+	property_override("dalvik.vm.heapgrowthlimit", "192m");
+	property_override("dalvik.vm.heapsize", "512m");
+	property_override("dalvik.vm.heapmaxfree", "16m");
+	property_override("dalvik.vm.heapminfree", "4m");
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+	property_override("dalvik.vm.heapstartsize", "8m");
+	property_override("dalvik.vm.heaptargetutilization", "0.7");
+	property_override("dalvik.vm.heapgrowthlimit", "192m");
+	property_override("dalvik.vm.heapsize", "512m");
+	property_override("dalvik.vm.heapmaxfree", "8m");
+	property_override("dalvik.vm.heapminfree", "2m");
+    }
 }
 
-void vendor_load_properties()
-{
-	load_dalvik_properties();
+void vendor_load_properties() {
+    load_dalvik_properties();
 
-	// fingerprint
-	property_override("ro.build.description", "redfin-user 11 RQ1A.201205.010 6953398 release-keys");
-	property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys");
+    // fingerprint
+    property_override("ro.build.description", "redfin-user 11 RQ1A.201205.010 6953398 release-keys");
+    property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys");
 	
-	// Magisk Hide
-	property_override("ro.boot.verifiedbootstate", "green");
-	property_override("ro.boot.vbmeta.device_state", "locked");
-	property_override("ro.boot.veritymode", "enforcing");
-	property_override("ro.build.type", "user");
-	property_override("ro.build.tags", "release-keys");
+    // Magisk Hide
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.build.type", "user");
+    property_override("ro.build.tags", "release-keys");
 }
