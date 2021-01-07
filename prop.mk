20520f74f50172c62b2c56b5c7cdba5dd35a32c1
GeoPD <geoemmanuelpd2001@gmail.com>
2021-01-07 16:29:53 +0530

    sakura: Remove zygote preforking (USAP pool)
    - Enabled by default from source
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -229,8 +229,3 @@ persist.vendor.usb.config.extra=none
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
-
-# Zygote preforking
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-persist.device_config.runtime_native.usap_pool_enabled=true
-
