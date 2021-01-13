94599684ce945dd74da4fd8cbe7072fc05b97fe4
GeoPD <geoemmanuelpd2001@gmail.com>
2021-01-13 13:46:52 +0530

    sakura: Remove ART debug for host or device
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -493,3 +493,11 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Don't build debug for host or device
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+USE_DEX2OAT_DEBUG := false
