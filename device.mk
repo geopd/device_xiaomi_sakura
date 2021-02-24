50f3939e16eb5fe16bdaf47cb5a1def787b30f73
Quallenauge <Hamsi2k@freenet.de>
2021-02-24 19:40:28 +0100

    sakura: rro: Exclude CarrierConfig overlays from RRO
    They doesn't work right, because xml attributes where
    stripped down, and leading zeros are removed.
    
    E.g. instead of
     <carrier_config mcc="262" mnc="06">
    the values are modified and get
     <carrier_config mcc="262" mnc="6">
    
    This prevents matching of defined carrier definitions.
    
    Change-Id: I11efe716f9d2ea44bef80c15004d7a8cb71b58b9
    Signed-off-by: geopd <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 TARGET_USES_RRO := true
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
