1f9b470d5317db0226c77a3ba13fd87279f27f8a
GeoPD <geoemmanuelpd2001@gmail.com>
2021-03-27 14:42:22 +0530

    sakura: Build dependency camera packages for IMS
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -158,7 +158,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     vendor.qti.hardware.camera.device@1.0 \
     libdng_sdk.vendor \
     libgui_vendor
