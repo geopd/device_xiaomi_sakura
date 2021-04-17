5a7985bb4554133c2ca93d0182eccae05e9eed8a
GeoPD <geoemmanuelpd2001@gmail.com>
2021-04-17 17:37:33 +0530

    sakura: Drop lineage livedisplay and trust HALs
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -291,10 +291,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.msm8953
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
