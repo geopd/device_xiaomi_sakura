640da288b70e77ebb5eacca9b686282be9b729f7
GeoPD <geoemmanuelpd2001@gmail.com>
2021-05-14 15:16:25 +0530

    sakura: Update dependencies
    *Export official flag on build environment.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- dot_sakura.mk
+++ dot_sakura.mk
@@ -33,9 +33,6 @@ $(call inherit-product, device/xiaomi/sakura/device.mk)
 # Inherit some common dotOS stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
 
-# DotOS Official
-DOT_OFFICIAL := true
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
 PRODUCT_NAME := dot_sakura
