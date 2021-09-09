398239be01f0dfc06a51697b4731359a586c59ee
GeoPD <geoemmanuelpd2001@gmail.com>
2021-09-09 12:58:20 +0530

    sakura: rootdir: Fix theme and icon reset on reboot
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -429,9 +429,6 @@ esac
 # Remove recovery cache from persist
 rm -rf /mnt/vendor/persist/cache/recovery
 
-# Remove settings cache, avoids derps after dirty flash
-rm -rf /data/system/package_cache
-
 #
 # Make modem config folder and copy firmware config to that folder for RIL
 #
