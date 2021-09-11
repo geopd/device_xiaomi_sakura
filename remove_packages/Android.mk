5a0adfde20d74af329bb4d366e257947dd5fb3eb
GeoPD <geoemmanuelpd2001@gmail.com>
2021-09-11 23:19:06 +0530

    sakura: Remove more gapps packages
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup MyVerizonServices OBDM_Permissions SprintDM SprintHM VzwOmaTrigger YoutubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt arcore CalendarGooglePrebuilt CarrierSetup DevicePolicyPrebuilt DiagnosticsToolPrebuilt Drive GoogleCamera GoogleFeedback OBDM_Permissions MaestroPrebuilt Maps MicropaperPrebuilt PixelLiveWallpaperPrebuilt PixelWallpapers2020 PrebuiltGmail RecorderPrebuilt SafetyHubPrebuilt ScribePrebuilt WallpapersBReel2020 YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
