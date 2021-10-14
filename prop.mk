63e8642805ea9a3287e65aa4aa2220b7dd0642d0
Arvind Kumar <karvin@codeaurora.org>
2021-10-14 11:11:52 +0400

    sakura: Set manufacturer and soc properties
    Set soc model and manufacturer properties
    
    Change-Id: I9ecc1cb75a415f5df147d47234fd9d8b69e1e6b5
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -223,6 +223,11 @@ persist.vendor.vt.supported=1 \
 persist.vendor.sys.cnd.iwlan=1 \
 persist.vendor.cne.logging.qxdm=3974
 
+# Soc
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.soc.manufacturer=QTI \
+ro.soc.model=MSM8953
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true
