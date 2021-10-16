f6673a0f136ae0e0861edc1c9c4aab5913c03244
GeoPD <geoemmanuelpd2001@gmail.com>
2021-10-17 01:26:37 +0530

    sakura: Uprev android.hardware.radio to 1.5
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -420,9 +420,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
-    android.hardware.radio@1.3 \
-    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.secure_element@1.0 \
     librmnetctl \
