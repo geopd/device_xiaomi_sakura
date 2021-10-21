b491c9d49713ed5d6b326656c5190aaf0a346417
GeoPD <geoemmanuelpd2001@gmail.com>
2021-10-21 23:59:30 +0530

    sakura: Fallback to fingerprint blobs from sakura stock pie
    -Revert "[BLOBS]sakura: Pull fingerprint blobs from daisy V10.0.2.0.PDLMIFJ"
    -Revert "[BLOBS]sakura: Downgrade fingerprint firmwares to daisy V10.0.20.0.PDLMIXM"
    -Revert "[BLOBS]sakura: Roll back fingerprint blobs to daisy V10.0.20.0.PDLMIXM"
    -Revert "sakura: Build custom fingerprint HIDL"
    -Revert "sakura: biometrics: Cleanup Android.bp"
    -Revert "sakura: biometrics: Simplify fingerprint data handling"
    -Revert "sakura: biometrics: Hardcode fingerprint HALs list"
    -Revert "sakura: biometrics: Add custom fingerprint HAL loading logic"
    -Revert "sakura: biometrics: Rebrand for Xiaomi MSM8953"
    -Revert "sakura: Import fingerprint HIDL"
    
    *The log spam issue for fpc1020 fingerprint sensor is triggered randomly
     among users with the new blobs.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -224,10 +224,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
-
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
