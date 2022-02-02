25b648a48e203da1d156954f2ae2cd304ec325ca
GeoPD <geoemmanuelpd2001@gmail.com>
2022-02-02 22:13:06 +0530

    Revert "[BLOBS]msm8953-common: Update wcnss_service from LA.UM.9.6.3.r1-04400-89xx.0"
    This reverts commit 30a700fae6a6df181ab274c085e6924e5491105b.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -487,6 +487,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy \
     libcld80211 \
+    libqmiservices_shim \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwpa_client \
